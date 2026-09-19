c544d91e51bf2652c3c557d4b53776cd977f9d95
Ocarina2211 <decescomatthieu@gmail.com>
2026-09-19 20:32:54 +0200

    y

---
+++ Banque.py
@@ -0,0 +1,29 @@
+comptes = {
+    "Matthieu": 1250,
+    "Lucas": 850,
+    "Emma": 2100
+}
+
+def afficher_les_comptes(comptes):
+    print("=== COMPTES ===")
+    print("" \
+    "")
+    for nom, argent in comptes.items():
+        print(nom, ":",argent, "€")
+
+
+def depot_argent(comptes):
+    compte = str(input("Quel est tom nom ? "))
+    if compte in comptes.keys():
+        ancien_montant = comptes[compte]
+        montant = float(input("Combien souhaitez-vous déposer sur votre compte ?"))
+        if montant > 0 :
+            comptes[compte] = ancien_montant + montant
+            print("Depot réussi ! ")
+            print("Votre compte est passé de ", ancien_montant, "€ à",comptes[compte], "€")
+        else :
+            print("Vous devez saisir une valeur supérieur à 0")
+    else :
+        print("Nous n'avons pas de compte à votre nom, veuillez réssayer")
+
+depot_argent(comptes)
