d4d309b3a5c5d66d96573fc0e5ded7ff38b68cd7
Matthieu De Cesco <decescomatthieu@gmail.com>
2026-09-25 11:53:06 +0200

    j 'ai avancer

--- RPG.py
+++ RPG.py
@@ -6,6 +6,22 @@ class Personnage :
         self.attaque = attaque 
         self.niveau = niveau
 
+    def presenter(self):
+        print("Je suis", self.nom)
+        print("J'ai", self.vie,"PV")
+        print("Mes attaques font ", self.attaque, "dégats")
+        print("Je suis niveau", self.niveau)
 
-joueur = Personnage("Matthieu", 100, 20, 1)
-print(joueur.nom)
+    def attaquer(self, cible):
+        cible.vie -= self.attaque
+        if cible.vie <= 0 :
+            print(cible.nom, "est mort, la honte")
+        
+   
+
+
+joueur1 = Personnage("Matthieu", 100, 20, 1)
+ennemi1 = Personnage("Goblin", 100, 30, 1)
+joueur1.presenter()
+joueur1.attaquer(ennemi1)
+ennemi1.presenter()
