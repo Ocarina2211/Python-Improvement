f473482b8735c798fec166e41653066d5f1416e7
Matthieu De Cesco <decescomatthieu@gmail.com>
2026-09-26 00:24:25 +0200

    gestion du shield

--- RPG.py
+++ RPG.py
@@ -7,19 +7,27 @@ class Personnage :
         self.niveau = niveau
         self.vie_max = vie_max
 
+
     def presenter(self):
         print("Je suis", self.nom)
-        print("J'ai", self.vie,"PV")
+        if self.vie > 0 :
+            print("J'ai", self.vie,"PV")
+        else :
+            print("Je suis mort")
         print("Mes attaques font", self.attaque, "dégats")
         print("Je suis niveau", self.niveau)
 
+
+    def recevoir_degats(self, degats):
+        if self.vie > degats :
+            self.vie -= degats
+        else :
+            self.vie = 0
+
+
     def attaquer(self, cible):
-        cible.vie -= self.attaque
-        print(self.nom, "attaque", cible.nom, "!!!")
-        if cible.vie <= 0 :
-            print(cible.nom, "est mort, la honte")
-            cible.vie = 0
-        print(cible.nom, " : ",cible.vie,"/",cible.vie_max, "PV")
+        cible.recevoir_degats(self.attaque)
+
 
     def heal(self, heal):
         if self.vie + heal <= self.vie_max :
@@ -37,14 +45,29 @@ class Personnage :
 
 
 class Guerrier(Personnage):
-    def __init__(self, nom, vie, attaque, niveau, vie_max, sheild):
+    def __init__(self, nom, vie, attaque, niveau, vie_max, shield):
         super().__init__(nom, vie, attaque, niveau, vie_max)
-        self.sheild = sheild
+        self.shield = shield
 
     def presenter(self):
         super().presenter()
-        print("J'ai",self.sheild, " de sheild")
+        if self.shield > 0 :
+            print("J'ai",self.shield, " de shield")
+        else :
+            print ("J'ai plus de shield")
 
+
+    def recevoir_degats(self, degats):
+        if self.shield > 0:
+            degats = degats / 2
+            self.shield -= degats
+
+        if self.shield < 0:
+            self.shield = 0
+
+        super().recevoir_degats(degats)
+
+    
 
 
 
@@ -52,6 +75,7 @@ class Mage(Personnage):
     def __init__(self, nom, vie, attaque, niveau, vie_max, mana):
         super().__init__(nom, vie, attaque, niveau, vie_max)
         self.mana = mana
+
     def presenter(self):
         super().presenter()
         print("J'ai", self.mana, " de mana ")
@@ -77,12 +101,12 @@ def combat(joueur1, joueur2):
     
         
 
-joeur1 = Personnage("Matthieu", 100, 20, 1, 100)
+joueur1 = Personnage("Matthieu", 100, 20, 1, 100)
 ennemi1 = Personnage("Goblin", 100, 30, 1, 100)
 guerrier = Guerrier("Brutus", 100, 40, 1, 130, 25 )
-harry = Mage("Harry", 100, 35, 1, 100, 70)
+harry = Mage("Harry", 100, 20, 1, 100, 70)
 
 
 guerrier.presenter()
-harry.presenter()
-
+harry.attaquer(guerrier)
+guerrier.presenter()
