10607694a0ce3356567cb5a100c4f991b9ba85ac
Ocarina2211 <decescomatthieu@gmail.com>
2026-09-25 22:19:55 +0200

    guerrier

--- RPG.py
+++ RPG.py
@@ -15,10 +15,11 @@ class Personnage :
 
     def attaquer(self, cible):
         cible.vie -= self.attaque
-        print(self.nom, "attaque", cible.nom, "!!! Il lui infligeant", self.attaque, "dégats")
+        print(self.nom, "attaque", cible.nom, "!!!")
         if cible.vie <= 0 :
             print(cible.nom, "est mort, la honte")
             cible.vie = 0
+        print(cible.nom, " : ",cible.vie,"/",cible.vie_max, "PV")
 
     def heal(self, heal):
         if self.vie + heal <= self.vie_max :
@@ -28,18 +29,35 @@ class Personnage :
             print("Power heal :", heal)
         else :
             print("PV Avant Heal : ", self.vie)
-            self.vie = 100
+            self.vie = self.vie_max
             print("PV actuel : ", self.vie)
             print("Power heal :", heal)
         
    
 def combat(joueur1, joueur2):
-    while joueur1.vie > 0 or joueur2.vie > 0 :
-        tour = 1
+    tour = 1
+    while joueur1.vie > 0 and joueur2.vie > 0 :
         print(" --- TOUR ",tour, "---")
+        print("" \
+        "")
+        joueur1.attaquer(joueur2)
+        print("" \
+        "")
+        if joueur2.vie > 0 :
+            joueur2.attaquer(joueur1)
+        print("" \
+        "")
+        tour += 1
+    if joueur1.vie == 0 :
+        print(joueur2.nom, "a détruit", joueur1.nom,"!!!")
+    else :
+        print(joueur1.nom, "a détruit", joueur2.nom,"!!!")
+    
         
 
-joueur1 = Personnage("Matthieu", 100, 50, 1, 100)
+joueur1 = Personnage("Matthieu", 100, 20, 1, 100)
 ennemi1 = Personnage("Goblin", 100, 30, 1, 100)
 
+
+combat(joueur1, ennemi1)
 
